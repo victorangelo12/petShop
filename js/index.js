e7445efd880904c605655c65764dccd7f7438cce
victorangelo12 <victorangelosk8@gmail.com>
2024-01-31 10:36:09 -0300

    realizado comentários para melgor explicações

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 var elementosDuvida = document.querySelectorAll('.duvida')
 
+// o query selector all ele pega a classe dúvida criada
 
 elementosDuvida.forEach(function (duvida) {
     duvida.addEventListener('click', function(){
         duvida.classList.toggle('ativa')
-    })
+    })  
 })
 
+// add o tipo de evento que vc quer ouvir click, passar o mouse, etc 
+//class list alterna a presença de uma classe específica no conjunto de classes de um elemento. Se a classe estiver presente, ela será removida
 
